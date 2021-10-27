80bd3a158626576aee55ddb03c13459156f3bd52
Justin Desoto <justin.desoto@yahoo.com>
2021-10-27 09:02:25 -0600

    V1

---
+++ script.js
@@ -0,0 +1,20 @@
+let myUl = [{
+    name: 'Bob'
+    },
+{
+    name: 'Jim'
+    },
+{
+     name: 'Sarah'
+    },
+{
+     name: 'Matt'
+    },
+{
+     name: 'Suzy'
+    },
+{
+    name: 'Mary'
+}]
+
+console.log(myUl)
